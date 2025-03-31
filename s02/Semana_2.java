8d9ac2a0a0a2dddab1a8b7a5e992fc32918f4739
Alvaro <alvarohenry@gmail.com>
2025-03-30 22:09:35 -0500

    semana 2

---
+++ s02/Semana_2.java
@@ -0,0 +1,74 @@
+package pe.usil.estudiante.semana_2;
+
+public class Semana_2 {
+
+    public static void main(String[] args) {
+        /*
+        int max = 70;
+        int sum = 0;
+        
+        for(int k = 1; k <= max; k++) {
+            sum = sum + k;
+        }        
+        System.out.println("La suma de los 70 primeros numeros es: " + sum);        
+        System.out.println("La suma de los 70 primeros numeros CON FORMULA es: " + (max * (max+1))/2 );
+        */
+        /*
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        */
+        /*
+        try {
+            System.out.println("Inicializando la variable n1 con el valor maximo "
+                    + "de enteros + 1");
+            int n1 = Integer.MAX_VALUE + 1;
+            System.out.println("El numero es: " + n1);
+        } catch (Exception e) {
+            System.out.println(e);
+        }*/
+        /*
+        System.out.println(2.5 / 0.0);
+        System.out.println(-2.5 / 0.0);
+        System.out.println(0.0 / 0.0);
+        System.out.println((2.5 / 0.0) / 0.0);
+        */
+        //int n = 10;
+        //System.out.println(n / 0);
+        /*double x = 10.0;
+        System.out.println(x / 0);*/
+        /*
+        String tipoDulce = "cookie";
+        char primeraLetra = tipoDulce.charAt(0);
+        System.out.println(primeraLetra + " es la primera letra de " + tipoDulce);
+        
+        String mensaje = "java";
+        for(int i = 0; i < mensaje.length();  i++){
+            char c = mensaje.charAt(i);
+            System.out.println("Caracter posicion: " + i + " es: " + c);
+        }
+        
+        int valorX = 3;
+        int valorY = 5;
+        
+        String punto = "(" + valorX + ", " + valorY + ")"; // (3, 5)
+        System.out.println(punto);
+        */
+        /*
+        String course = "Programacion Java";        
+        System.out.println(course.length());
+        System.out.println(course.indexOf('i'));
+        System.out.println(course.substring(7, 11));
+        System.out.println(course.toLowerCase());
+        System.out.println(course.toUpperCase());
+        System.out.println(course.substring(13));
+        */
+        //String myCar;
+        //String myCar;
+        //String myCar = "Toyota";
+        //Integer i = "";
+        //String s = (String)"";
+        
+        //final double PI = 3.1416;
+        //PI = 4;
+    }
+}
